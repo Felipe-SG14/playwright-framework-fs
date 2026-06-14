18b1ca961315891eda207a4e6e3c662e39ad2069
Felipe-SG14 <69222920+Felipe-SG14@users.noreply.github.com>
2026-06-13 18:31:20 -0600

    Adding JUnit report

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { 
+      outputFolder: 'playwright-report', 
+      open: 'never' 
+    }],
+    ['junit', { 
+      outputFile: 'test-results/junit-report.xml',
+      stripANSIControlSequences: true // Limpia caracteres raros de color de la consola en el XML
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
